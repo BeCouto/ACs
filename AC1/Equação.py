22db764eca8ef6957049135920c0afe119a0acb1
Gabriel Couto <161481104+BeCouto@users.noreply.github.com>
2024-03-11 16:41:12 -0300

    Bissexto_e_Equação.py

--- AC1/Equação.py
+++ AC1/Equação.py
@@ -17,3 +17,10 @@ x1 = (-b + delta**0.5) / (2*a)
 x2 = (-b - delta**0.5) / (2*a)
 print("A primeira raiz da equação é: ", x1)
 print("A segunda raiz da equação é: ", x2)
+
+# AC1
+# Calcular se o ano é Bissexto ou não
+# Aluno: Gabriel Couto Barros
+
+ano = int(input("Informe o ano: "))
+print((ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0))
